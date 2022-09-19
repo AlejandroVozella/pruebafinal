0b5cdc43aaefdba177c3c2eba5f00b7c2f5c062f
Alejandro Vozella <alejandrovozella@gmail.com>
2022-09-18 22:11:13 -0300

    Primer Commit del Proyecto

--- src/Shared/Proyecto/Proyecto.js
+++ src/Shared/Proyecto/Proyecto.js
@@ -1,27 +1,21 @@
 import React from "react";
-import './style.css';
+import "./style.css";
 //import MaterialTable from 'material-table';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Proyecto = () => {
+  return (
+    <div>
+      <table className="table table-dark">
+        <thead></thead>
+        <tbody>
+          <tr className="alumnos">
+            <h4></h4>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-  
-    
-    return ( 
-        <div>
-            <table className="table table-dark">
-              <thead >
-                
-              </thead>
-              <tbody>
-                
-                
-                <tr className="alumnos"><h4>Alejandro Vozella</h4></tr>
-              </tbody>
-            </table>
-           
-        </div>
-     );
-}
- 
 export default Proyecto;
