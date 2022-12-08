6fff1161b99cd5ae3fc79245f543ef0bf45f859a
Alejandro Vozella <alejandrovozella@gmail.com>
2022-12-08 18:44:46 -0300

    Modificacion Footer

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import './style.css';
 const Footer = ()=>{
     return(
         <div className="footer">
-            <h3>CP AV Tecnology</h3>
+            <h3>AV Tecnology</h3>
             <p>Copyright &copy; 2022</p>
         </div>
     )
